6375f3cb95fe61a618baaf04c15c7de6b51d94ff
Tommy Kristiansen <tommy.kristiansen@nav.no>
2020-03-23 21:20:07 +0100

    assigned topic

--- src/main/kotlin/no/nav/pdlsf/StsService.kt
+++ src/main/kotlin/no/nav/pdlsf/StsService.kt
@@ -21,6 +21,7 @@ private fun fetchNewToken(): StsAccessTokenBase = Http.client.invoke(
                 .header("Content-type", ContentType.APPLICATION_FORM_URLENCODED.toHeaderValue())
                 .query("grant_type", "client_credentials"))
                 .query("scope", "openid")
+                .body("")
 ).let { response ->
     when (response.status) {
         Status.OK -> json.parse<StsAccessToken>(response.bodyString())
