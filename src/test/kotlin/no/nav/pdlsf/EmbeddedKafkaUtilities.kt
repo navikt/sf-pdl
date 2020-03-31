a61f0dcc6d03c9051817b3d6ec483da3a3c7e44b
torsteinnesby <ttnesby@gmail.com>
2020-03-31 12:03:30 +0200

    Added support for log compaction and relevant tests

--- src/test/kotlin/no/nav/pdlsf/EmbeddedKafkaUtilities.kt
+++ src/test/kotlin/no/nav/pdlsf/EmbeddedKafkaUtilities.kt
@@ -1,10 +1,15 @@
 package no.nav.pdlsf
 
+import com.google.protobuf.InvalidProtocolBufferException
 import no.nav.common.KafkaEnvironment
+import no.nav.pdlsf.proto.PdlSfValuesProto
+import org.apache.kafka.clients.admin.AlterConfigOp
+import org.apache.kafka.clients.admin.ConfigEntry
 import org.apache.kafka.common.acl.AccessControlEntry
 import org.apache.kafka.common.acl.AclBinding
 import org.apache.kafka.common.acl.AclOperation
 import org.apache.kafka.common.acl.AclPermissionType
+import org.apache.kafka.common.config.ConfigResource
 import org.apache.kafka.common.resource.PatternType
 import org.apache.kafka.common.resource.ResourcePattern
 import org.apache.kafka.common.resource.ResourceType
@@ -34,4 +39,42 @@ internal fun KafkaEnvironment.addConsumerToTopic(username: String, topic: String
             }
     )
     ke
+}
+
+internal fun KafkaEnvironment.setLogCompaction(topicName: String) = this.let { ke ->
+    val configResource = ConfigResource(ConfigResource.Type.TOPIC, topicName)
+    val configLogCompact = listOf(
+            AlterConfigOp(
+                    ConfigEntry("cleanup.policy", "compact"),
+                    AlterConfigOp.OpType.SET
+            )
+    )
+    val configReq: Map<ConfigResource, Collection<AlterConfigOp>> = mapOf(configResource to configLogCompact)
+
+    ke.adminClient?.incrementalAlterConfigs(configReq)
+    ke
+}
+
+internal fun ByteArray.protobufSafeParseKey(): PdlSfValuesProto.SfObjectEventKey = this.let { ba ->
+    try {
+        PdlSfValuesProto.SfObjectEventKey.parseFrom(ba)
+    } catch (e: InvalidProtocolBufferException) {
+        PdlSfValuesProto.SfObjectEventKey.getDefaultInstance()
+    }
+}
+
+internal fun ByteArray.protobufSafeParseAccount(): PdlSfValuesProto.AccountValue = this.let { ba ->
+    try {
+        PdlSfValuesProto.AccountValue.parseFrom(ba)
+    } catch (e: InvalidProtocolBufferException) {
+        PdlSfValuesProto.AccountValue.getDefaultInstance()
+    }
+}
+
+internal fun ByteArray.protobufSafeParsePerson(): PdlSfValuesProto.PersonValue = this.let { ba ->
+    try {
+        PdlSfValuesProto.PersonValue.parseFrom(ba)
+    } catch (e: InvalidProtocolBufferException) {
+        PdlSfValuesProto.PersonValue.getDefaultInstance()
+    }
 }
