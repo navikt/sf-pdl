a42f48066c4d7bce5fa39177393c8c2eb1b21f60
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2020-06-16 08:35:46 +0200

    Personfilter added

--- src/main/kotlin/no/nav/sf/pdl/Work.kt
+++ src/main/kotlin/no/nav/sf/pdl/Work.kt
@@ -2,18 +2,24 @@ package no.nav.sf.pdl
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.parse
+import kotlinx.serialization.serializer
 import mu.KotlinLogging
 import no.nav.pdlsf.proto.PersonProto
 import no.nav.sf.library.AKafkaConsumer
 import no.nav.sf.library.AKafkaProducer
+import no.nav.sf.library.AVault
 import no.nav.sf.library.AllRecords
 import no.nav.sf.library.AnEnvironment
 import no.nav.sf.library.KafkaConsumerStates
 import no.nav.sf.library.PROGNAME
 import no.nav.sf.library.getAllRecords
+import no.nav.sf.library.json
 import no.nav.sf.library.send
 import no.nav.sf.library.sendNullValue
+import no.nav.sf.pdl.FilterBase.Companion.fromJson
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
@@ -23,29 +29,76 @@ private val log = KotlinLogging.logger {}
 
 // Work environment dependencies
 const val EV_kafkaProducerTopic = "KAFKA_PRODUCER_TOPIC"
+
+// Work vault dependencies
+const val VAULT_workFilter = "WorkFilter"
+
 val kafkaPersonTopic = AnEnvironment.getEnvOrDefault(EV_kafkaProducerTopic, "$PROGNAME-producer")
 
 data class WorkSettings(
     val kafkaConsumerPerson: Map<String, Any> = AKafkaConsumer.configBase + mapOf<String, Any>(
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java
-    ),
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java,
+    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ByteArrayDeserializer::class.java
+),
     val kafkaProducerPerson: Map<String, Any> = AKafkaConsumer.configBase + mapOf<String, Any>(
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to ByteArraySerializer::class.java,
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to ByteArraySerializer::class.java
-    ),
+    ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to ByteArraySerializer::class.java,
+    ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to ByteArraySerializer::class.java
+),
     val kafkaConsumerPdl: Map<String, Any> = AKafkaConsumer.configBase + mapOf<String, Any>(
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java
-    )
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java
+),
+    val filter: FilterBase = fromJson(AVault.getSecretOrDefault(VAULT_workFilter)),
+    val prevFilter: FilterBase = FilterBase.Missing
 )
 
 sealed class ExitReason {
+    object NoFilter : ExitReason()
     object NoKafkaProducer : ExitReason()
     object NoKafkaConsumer : ExitReason()
     object NoEvents : ExitReason()
     object NoCache : ExitReason()
     object Work : ExitReason()
+}
+
+/**
+ * Filter persons
+ * Only LIVING persons in listed regions and related municipals will be transferred to Salesforce
+ * iff empty list of municipal  - all living persons in that region
+ * iff non-empty municipals - only living person in given region AND municipals will be transferred
+*/
+sealed class FilterBase {
+    object Missing : FilterBase()
+
+    @Serializable
+    data class Exists(
+        val regions: List<RegionBase.Exists>
+    ) : FilterBase() {
+
+        fun approved(p: PersonSf): Boolean {
+            return regions.any { !p.doed &&
+                    it.region == p.region && (it.municipals.isEmpty() || it.municipals.contains(p.kommunenummer)) }
+        }
+    }
+
+    companion object {
+        fun fromJson(data: String): FilterBase = runCatching { json.parse(Exists.serializer(), data) }
+                .onFailure {
+                    log.error { "Parsing of person filter in vault failed - ${it.localizedMessage}" }
+                }
+                .getOrDefault(Missing)
+    }
+}
+
+sealed class RegionBase {
+
+    object Missing : RegionBase()
+
+    @Serializable
+    data class Exists(
+        val region: String,
+        val municipals: List<String> = emptyList()
+    ) : RegionBase()
 }
 
 /**
@@ -86,6 +139,15 @@ internal fun work(ws: WorkSettings): Pair<WorkSettings, ExitReason> {
     log.info { "bootstrap work session starting" }
 
     /**
+     * Check - no filter means nothing to transfer, leaving
+     */
+    if (ws.filter is FilterBase.Missing) {
+        log.warn { "No active filter for activities, leaving" }
+        return Pair(ws, ExitReason.NoFilter)
+    }
+    val personFilter = ws.filter as FilterBase.Exists
+
+    /**
      * Check - no cache means no answer for a new event;
      * - is a new activity id
      * - existing activity id, but updated activity details
@@ -106,7 +168,8 @@ internal fun work(ws: WorkSettings): Pair<WorkSettings, ExitReason> {
 
         val kafkaConsumerPdl = AKafkaConsumer<String, String>(
                 config = ws.kafkaConsumerPdl,
-                fromBeginning = false
+                fromBeginning = ((ws.filter.hashCode() != ws.prevFilter.hashCode()) || cache.isEmpty)
+                        .also { log.info { "Start from beginning due to filter change or empty cache" } }
         )
         exitReason = ExitReason.NoKafkaConsumer
 
@@ -119,11 +182,14 @@ internal fun work(ws: WorkSettings): Pair<WorkSettings, ExitReason> {
             exitReason = ExitReason.Work
                 val results = cRecords.map { cr ->
                     if (cr.value() == null) {
+                        log.info { "debug Consumer record value lacking (tombestone)}" }
                         val personTombestone = PersonTombestone(aktoerId = cr.key())
                         Pair(KafkaConsumerStates.IsOk, personTombestone)
                     } else {
+                        log.info { "debug Consumer record value consumed: ${cr.value()}" }
                         when (val query = cr.value().getQueryFromJson()) {
                             InvalidQuery -> {
+
                                 log.error { "Unable to parse topic value PDL" }
                                 Pair(KafkaConsumerStates.HasIssues, PersonInvalid) // TODO:: HasIssues, ignore when test
                             }
@@ -148,7 +214,7 @@ internal fun work(ws: WorkSettings): Pair<WorkSettings, ExitReason> {
 
                 if (areOk) {
                     log.info { "${results.size} consumer records resulted in number of Person ${results.filter { it.second is PersonSf }.count()}, Tombestone ${results.filter { it.second is PersonTombestone }.count()}, Invalid  ${results.filter { it.second is PersonInvalid }.count()}" }
-                    results.map {
+                    results.filter { it.second is PersonTombestone || (it.second is PersonSf && personFilter.approved(it.second as PersonSf)) }.map {
                         when (val personBase = it.second) {
                             is PersonTombestone -> {
                                 Pair<PersonProto.PersonKey, PersonProto.PersonValue?>(personBase.toPersonTombstoneProtoKey(), null)
--- src/test/kotlin/no/nav/sf/WorkTests.kt
+++ src/test/kotlin/no/nav/sf/WorkTests.kt
@@ -1,34 +1,81 @@
 package no.nav.sf
-import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotest.assertions.asClue
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import kotlinx.serialization.ImplicitReflectionSerializer
+import no.nav.sf.pdl.ExitReason
+import no.nav.sf.pdl.FilterBase
 import no.nav.sf.pdl.ObjectInCacheStatus
 import no.nav.sf.pdl.PersonSf
+import no.nav.sf.pdl.WorkSettings
 import no.nav.sf.pdl.exists
+import no.nav.sf.pdl.work
 
+@ExperimentalStdlibApi
+@ImplicitReflectionSerializer
 class WorkTests : StringSpec() {
 
     init {
-        val aktoerIdOne = "a1"
-        val personOne = PersonSf(identifikasjonsnummer = "1")
-        val aktoerIdTwo = "a2"
-        val personTwo = PersonSf(identifikasjonsnummer = "2")
-        val aktoerIdThree = "a3"
-        val personThree = PersonSf(identifikasjonsnummer = "3")
-
-        val aktoerIdNew = "a4"
-        val personNew = PersonSf(identifikasjonsnummer = "4")
-        val personUpdated = PersonSf(identifikasjonsnummer = "5")
-
-        val personCache = mapOf(
-                aktoerIdOne to personOne.hashCode(),
-                aktoerIdTwo to personTwo.hashCode(),
-                aktoerIdThree to personThree.hashCode()
-        )
-
         "Verify exists check on cache" {
+
+            val aktoerIdOne = "a1"
+            val personOne = PersonSf(identifikasjonsnummer = "1")
+            val aktoerIdTwo = "a2"
+            val personTwo = PersonSf(identifikasjonsnummer = "2")
+            val aktoerIdThree = "a3"
+            val personThree = PersonSf(identifikasjonsnummer = "3")
+
+            val aktoerIdNew = "a4"
+            val personNew = PersonSf(identifikasjonsnummer = "4")
+            val personUpdated = PersonSf(identifikasjonsnummer = "5")
+
+            val personCache = mapOf(
+                    aktoerIdOne to personOne.hashCode(),
+                    aktoerIdTwo to personTwo.hashCode(),
+                    aktoerIdThree to personThree.hashCode()
+            )
+
             personCache.exists(aktoerIdOne, personOne.hashCode()) shouldBe ObjectInCacheStatus.NoChange
             personCache.exists(aktoerIdNew, personNew.hashCode()) shouldBe ObjectInCacheStatus.New
             personCache.exists(aktoerIdOne, personUpdated.hashCode()) shouldBe ObjectInCacheStatus.Updated
+        }
+
+        "FilterPersonBase fromJson should work as expected" {
+
+            val invalidJson1 = """invalid json"""
+
+            val validJson = """
+                {
+                    "regions": [
+                        {
+                            "region" : "54" ,
+                            "municipals" : []
+                        } ,
+                        {
+                            "region": "18",
+                            "municipals": ["1804" , "1806"]
+                        } ]
+                }
+            """.trimIndent()
+
+            FilterBase.fromJson(invalidJson1)
+                    .shouldBeInstanceOf<FilterBase.Missing>()
+
+            FilterBase.fromJson(validJson)
+                    .shouldBeInstanceOf<FilterBase.Exists>().asClue {
+                        it.regions.isEmpty() shouldBe false
+                        it.regions[0].region shouldBe "54"
+                        it.regions[0].municipals shouldHaveSize 0
+                        it.regions[1].region shouldBe "18"
+                        it.regions[1].municipals shouldHaveSize 2
+                    }
+        }
+
+        "work should exit correctly for different situations - NoFilter" {
+
+            work(WorkSettings(filter = FilterBase.Missing)).second.shouldBeInstanceOf<ExitReason.NoFilter>()
         }
     }
 }
