b1870ace26bc411234833c22b201c8add7b3ae56
Tommy Kristiansen <tommy.kristiansen@nav.no>
2020-06-19 09:54:37 +0200

    deploy to prod

--- src/main/kotlin/no/nav/sf/pdl/Work.kt
+++ src/main/kotlin/no/nav/sf/pdl/Work.kt
@@ -277,9 +277,7 @@ internal fun work(ws: WorkSettings): Pair<WorkSettings, ExitReason> {
 
             exitReason = ExitReason.Work
             val results = cRecords.map { cr ->
-                // log.info { "Topic value - ${cr.value()}" }
                 if (cr.value() == null) {
-                    // log.info { "debug Consumer record value lacking (tombestone)" }
                     val personTombestone = PersonTombestone(aktoerId = cr.key())
                     workMetrics.noOfTombestone.inc()
                     Pair(KafkaConsumerStates.IsOk, personTombestone)
