21e2b31320f05c300ba29e52dd2b710f42b0852c
Björn Hägglund <bjornhagglund@Bjorns-MacBook-Pro.local>
2020-06-19 10:52:17 +0200

    Metrics in cache fix and deploy to prod

--- src/main/kotlin/no/nav/sf/pdl/Work.kt
+++ src/main/kotlin/no/nav/sf/pdl/Work.kt
@@ -194,10 +194,22 @@ sealed class Cache {
             get() = map.isEmpty()
 
         internal fun isNewOrUpdated(item: Pair<PersonProto.PersonKey, PersonProto.PersonValue?>): Boolean = when {
-            !map.containsKey(item.first.aktoerId) -> true.also { log.info("debug cache new inc") ; workMetrics.cacheIsNewOrUpdated_noKey.inc() } // NY
-            item.second != null && map[item.first.aktoerId] != item.second.hashCode() -> true.also { log.info("debug cache update person inc") ; workMetrics.cacheIsNewOrUpdated_differentHash.inc() } // Updated
-            item.second == null && map[item.first.aktoerId] != null -> true.also { log.info("debug cache update tombestone inc") ; workMetrics.cacheIsNewOrUpdated_existing_to_tombestone.inc() } // Tombestone
-            else -> false.also { log.info("debug cache blocked (present in cache)") ; workMetrics.cacheIsNewOrUpdated_no_blocked.inc() }
+            !map.containsKey(item.first.aktoerId) -> {
+                workMetrics.cacheIsNewOrUpdated_noKey.inc()
+                true
+            }
+            item.second != null && map[item.first.aktoerId] != item.second.hashCode() -> {
+                workMetrics.cacheIsNewOrUpdated_differentHash.inc()
+                true
+            } // Updated
+            item.second == null && map[item.first.aktoerId] != null -> {
+                workMetrics.cacheIsNewOrUpdated_existing_to_tombestone.inc()
+                true
+            } // Tombestone
+            else -> {
+                workMetrics.cacheIsNewOrUpdated_no_blocked.inc()
+                false
+            }
         }
     }
 
